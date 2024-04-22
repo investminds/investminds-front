85bd418177c2572cb3c459e3834a23460be2d11d
mibasFerraz <lucasllff@gmail.com>
2024-04-22 19:23:03 -0300

    sim

---
+++ README.md
@@ -0,0 +1,9 @@
+# RaceIt
+
+Projeto de TCC do RaceIt
+
+## Para rodar use os seguintes comandos:
+
+npm install
+npm start
+
---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react"
+import "./navbar.css"
+import { Link } from "react-router-dom"
+import logo from "../../assets/logo.svg"
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='navbar'>
+
+        <img src={logo} className='logo' alt="logo do raceit"/>
+        
+        <ul className={"nav-links"}>
+          <Link to='/' className='home'>
+            <li>Home</li>
+          </Link>
+          <Link to='/about' className='about'>
+            <li>Sobre</li>
+          </Link>
+          <Link to='/contact' className='contact'>
+            <li>Contato</li>
+          </Link>
+        </ul>
+
+        <ul className={"nav-links"}>
+          <Link to='/login' className='login'>
+            <li>Login</li>
+          </Link>
+          <Link to='/signup' className='signup'>
+            <li>Cadastrar</li>
+          </Link>
+        </ul>        
+      </nav>
+    </>
+  )
+}
+export default Navbar
