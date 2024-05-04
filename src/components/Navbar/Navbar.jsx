5d58cd8713fe21522726b3e031dd068ea96f3827
Nicalo Ribeiro <nicalo63@gmail.com>
2024-05-04 01:59:31 -0300

    Locale switcher created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,37 @@
-import "./navbar.css"
-import { Link } from "react-router-dom"
-import logo from "../../assets/logo.svg"
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import LocaleSwitcher from "../LocaleSwitcher";
 
 const Navbar = () => {
   return (
     <>
-      <nav className='navbar'>
+      <nav className="px-8 navbar">
+        <img src={logo} className="logo" alt="logo do raceit" />
 
-        <img src={logo} className='logo' alt="logo do raceit"/>
-        
         <ul className={"nav-links"}>
-          <Link to='/' className='home'>
+          <Link to="/" className="home">
             <li>Home</li>
           </Link>
-          <Link to='/about' className='about'>
+          <Link to="/about" className="about">
             <li>Sobre</li>
           </Link>
-          <Link to='/contact' className='contact'>
+          <Link to="/contact" className="contact">
             <li>Contato</li>
           </Link>
         </ul>
 
         <ul className={"nav-links"}>
-          <Link to='/login' className='login'>
+          <Link to="/login" className="login">
             <li>Login</li>
           </Link>
-          <Link to='/signup' className='signup'>
+          <Link to="/signup" className="signup">
             <li>Cadastrar</li>
           </Link>
-        </ul>        
+          <LocaleSwitcher />
+        </ul>
       </nav>
     </>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
