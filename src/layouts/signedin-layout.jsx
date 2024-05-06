20601c3e4a651a3fbcfe0213a3e99c7ab4eb4ef4
Nicalo Ribeiro <nicalo63@gmail.com>
2024-05-06 00:21:55 -0300

    feat: layout fixed

--- src/layouts/signedin-layout.jsx
+++ src/layouts/signedin-layout.jsx
@@ -1,16 +1,14 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar/Navbar";
+import Navbar from "../components/Navbar";
 
 const SignedInLayout = () => {
   return (
-    <>
+    <div className="bg-gray-100 min-h-svh">
       <Navbar />
-      <div className="flex justify-center bg-gray-100 h-svh">
-        <div className="container flex-grow py-4">
-          <Outlet />
-        </div>
+      <div className="px-4 py-4 overflow-auto lg:container lg:mx-auto lg:px-12">
+        <Outlet />
       </div>
-    </>
+    </div>
   );
 };
 
