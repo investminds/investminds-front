4f7b59d9ed0aa5d473a4220091764e1a846ed0ec
mibasFerraz <lucasllff@gmail.com>
2024-06-19 10:34:07 -0300

    cadastro meta

--- src/pages/Facebook/Facebook.jsx
+++ src/pages/Facebook/Facebook.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import {  LoginButton, Page, useFacebook, useLoginStatus, useProfile } from 'react-facebook';
+import { LoginButton, useFacebook, useLoginStatus, useProfile } from 'react-facebook';
 
 const APP_ID = import.meta.env.VITE_FACEBOOK_APP_ID;
 
@@ -8,39 +8,43 @@ const Facebook = () => {
 
   async function handleSuccess(response) {
     try {
-      console.log(`response`, response)
-      const { authResponse } = response
-      const { userID} = authResponse
-      console.log(authResponse)
+      console.log(`response`, response);
+      const { authResponse } = response;
+      const { userID } = authResponse;
+      console.log(authResponse);
       FB.api(
         `/${userID}/accounts`,
         function (response) {
           if (response && !response.error) {
-            console.log('pages', response)
+            console.log('pages', response);
           }
         }
-    )
+      );
     } catch (error) {
-      console.log(error);     }
+      console.log(error);
+    }
   }
 
   function handleError(error) {
     console.log(error);
   }
-  
+
   return (
-    <>
-      <section className="hero">
-        
-        <LoginButton
-          scope="public_profile,email,pages_show_list,pages_read_engagement,pages_manage_posts"
-          onError={handleError}
-          onSuccess={handleSuccess}
-        >
-          Login via Facebook
-        </LoginButton>
-      </section>
-    </>
+    <section className="hero min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
+        <h2 className="text-2xl font-bold mb-4 text-center">Cadastre sua conta Meta</h2>
+        <div className="flex justify-center">
+          <LoginButton
+            scope="public_profile,email,pages_show_list,pages_read_engagement,pages_manage_posts"
+            onError={handleError}
+            onSuccess={handleSuccess}
+            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          >
+            Login via Facebook
+          </LoginButton>
+        </div>
+      </div>
+    </section>
   );
 };
 export default Facebook;
