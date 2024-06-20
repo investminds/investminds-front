8e765cba1a66d5530c462ceb89ec1e2baee5ba52
Nicalo Ribeiro <nicalo63@gmail.com>
2024-06-20 18:58:27 -0300

    fix: using api env

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
+const AUTH_API_BASE_URL = import.meta.env.VITE_AUTH_API_URL;
+
 const facebookLogin = async (user) => {
   try {
-    const res = await axios.post(
-      "http://localhost:3030/api/facebook/login",
-      user
-    );
+    const res = await axios.post(`${AUTH_API_BASE_URL}/facebook/login`, user);
     return res;
   } catch (error) {
     console.log(error);
