5513b0e485a5269f1d5d2cf6ca9eeff4bb7ce488
mibasFerraz <lucasllff@gmail.com>
2024-07-02 17:43:20 -0300

    feat: adding auth context on other files

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import "./config/translator.js";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
 import { FacebookProvider } from "react-facebook";
+import { AuthProvider } from "./context/auth-context.jsx";
 const APP_ID = import.meta.env.VITE_FACEBOOK_APP_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <FacebookProvider appId={APP_ID} version="v20.0">
       <Provider store={store}>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </Provider>
     </FacebookProvider>
   </React.StrictMode>
