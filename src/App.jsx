e7266923c9ea7740c1d7da9ab8370185e1ea6a9b
Nicalo Ribeiro <nicalo63@gmail.com>
2024-09-23 20:28:44 -0300

    fix: app entrypoint fixed
    Co-authored-by: Lucas Lima <lucasllff@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 // app.jsx
-import { useAuth } from './context/auth-context';
-import { SignedInRoutes, SignedOffRoutes } from './routes';
+import { useLayoutEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "./store/reducers/user";
+import { jwtDecode } from "jwt-decode";
+import { RouterProvider } from "react-router-dom";
+import signedOffRouter from "./routes/signedoff-routes";
+import signedInRouter from "./routes/signedin-routes";
+
+const RoutesMapper = (role) => {
+  switch (role) {
+    case "consumer":
+      return signedInRouter.consumerRouter;
+    case "advertiser":
+      return signedInRouter.advertiserRouter;
+    default:
+      return signedOffRouter;
+  }
+};
 
 const App = () => {
-  const { isAuthenticated } = useAuth();
+  const loggedUser = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-  return isAuthenticated ? <SignedInRoutes /> : <SignedOffRoutes />;
+  useLayoutEffect(() => {
+    const { jwt } = loggedUser;
+    const token = localStorage.getItem("access_token");
+    if (!token && !jwt) return;
+    const userInfo = jwtDecode(token);
+    dispatch(setUser({ user: { ...userInfo, jwt: token } }));
+  }, []);
+
+  return <RouterProvider router={RoutesMapper(loggedUser.role)} />;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,22 @@
+import App from "./App.jsx";
+import store from "./store/index.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { FacebookProvider } from "react-facebook";
 import "./index.css";
 import "./config/translator.js";
-import { Provider } from "react-redux";
-import store from "./store/index.js";
-import { FacebookProvider } from "react-facebook";
-import { AuthProvider } from "./context/auth-context.jsx";
+import "react-toastify/dist/ReactToastify.css";
+
 const APP_ID = import.meta.env.VITE_FACEBOOK_APP_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <FacebookProvider appId={APP_ID} version="v20.0">
       <Provider store={store}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <App />
+        <ToastContainer />
       </Provider>
     </FacebookProvider>
   </React.StrictMode>
